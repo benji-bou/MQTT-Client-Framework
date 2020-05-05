b2d5fc929cb693cc676432b2711644b4981165bf
Benjamin Bou-Achour <benjaminbouachour@MacBook-Pro-de-Benjamin.local>
2020-05-05 17:13:59 +0200

    rename package in config file

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "MQTT-Client-Framework",
+    name: "MQTTClient",
     platforms:[.iOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "MQTT-Client-Framework",
-            targets: ["MQTT-Client-Framework"]),
+		name: "MQTTClient",
+            	targets: ["MQTTClient"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,12 +20,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MQTT-Client-Framework",
+            name: "MQTTClient",
             dependencies: [],
-            path:"MQTTClient/MQTTClient"),
+            path:"MQTTClient/MQTTClient",  publicHeadersPath: ""),
         .testTarget(
-            name: "MQTT-Client-FrameworkTests",
-            dependencies: ["MQTT-Client-Framework"],
+            name: "MQTTClientTests",
+            dependencies: ["MQTTClient"],
             path:"MQTTClient/MQTTClientTests"),
     ]
 )
